e142eab394423c1b62d5252a13f3e616ecc5e36f
Ihor Yamshchykov <ihor.yamshchykov@gmail.com>
2020-06-12 23:27:23 +0300

    initial commit

---
+++ src/app/components/App/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => (
+  <>
+    <i class="fab fa-linkedin" style={{ fontSize: "2rem" }}></i>
+    <i class="fab fa-skype" style={{ fontSize: "2rem" }}></i>
+    <i class="fas fa-map-marker-alt" style={{ fontSize: "2rem" }}></i>
+    <div> test test test </div>
+  </>
+);
+
+export default App;
---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap";
+import "@fortawesome/fontawesome-free/js/all.min.js";
+import App from "./components/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
